7f0654441626ac2bdbc5af3c073f7c34ff218983
LuiggiP <164771607+LuiggiP@users.noreply.github.com>
2024-11-19 16:26:41 -0500

    changes to use sqlite and added buttons to execute functions like insertMovie, getAllMovies, updateMovie & deleteMovie

---
+++ lib/database_helper.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'models/Movie.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'movies.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE movies (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        overview TEXT,
+        popularity REAL,
+        realeaseDate TEXT,
+        isFavorite INTEGER
+      )
+    ''');
+  }
+
+  Future<int> insertMovie(Movie movie) async {
+    final db = await database;
+    return await db.insert('movies', movie.toMap());
+  }
+
+  Future<List<Movie>> getAllMovies() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('movies');
+
+    return List.generate(maps.length, (i) {
+      return Movie.fromJson(maps[i]);
+    });
+  }
+
+  Future<int> updateMovie(Movie movie) async {
+    final db = await database;
+    return await db.update(
+      'movies',
+      movie.toMap(),
+      where: 'id = ?',
+      whereArgs: [movie.id],
+    );
+  }
+
+  Future<int> deleteMovie(int id) async {
+    final db = await database;
+    return await db.delete(
+      'movies',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
